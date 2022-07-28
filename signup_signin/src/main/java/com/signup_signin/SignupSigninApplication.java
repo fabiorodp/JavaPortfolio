821d490c0f259ff208a8247323d0b767167db692
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2022-07-28 20:40:00 +0200

    project initialized

---
+++ signup_signin/src/main/java/com/signup_signin/SignupSigninApplication.java
@@ -0,0 +1,13 @@
+package com.signup_signin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupSigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupSigninApplication.class, args);
+	}
+
+}
