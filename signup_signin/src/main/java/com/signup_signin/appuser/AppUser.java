52d2b9194707d167be4c5099b51099d6df3b6c4f
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2022-07-29 13:35:20 +0200

    in progress

--- signup_signin/src/main/java/com/signup_signin/appuser/AppUser.java
+++ signup_signin/src/main/java/com/signup_signin/appuser/AppUser.java
@@ -1,44 +1,92 @@
 package com.signup_signin.appuser;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
+import javax.persistence.*;
+
+@Getter  //from spring lambok
+@Setter  //from spring lambok
+@EqualsAndHashCode  //from spring lambok
+@NoArgsConstructor  //from spring lambok
 public class AppUser implements UserDetails {
+
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String name, username, email, password;
+    @Enumerated(EnumType.STRING)
+    private AppUserRole appUserRole;
+    private Boolean locked, enabled;
+
+    public AppUser(
+            String name,
+            String username,
+            String email,
+            String password,
+            AppUserRole appUserRole,
+            Boolean locked,
+            Boolean enabled) {
+
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singletonList(authority);
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return !locked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return enabled;
     }
 }
---
+++ signup_signin/src/main/java/com/signup_signin/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.signup_signin.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
