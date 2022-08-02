69b66b922961af6baefd020c4768d45aa02806b0
Fabio Rodrigues Pereira <fabiorodp@gmail.com>
2022-08-02 12:34:43 +0200

    in progress

--- helloworld/src/main/java/com/helloworld/dao/PersonDao.java
+++ helloworld/src/main/java/com/helloworld/dao/PersonDao.java
@@ -3,6 +3,7 @@ package com.helloworld.dao;
 import com.helloworld.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -14,4 +15,7 @@ public interface PersonDao {
     }
 
     List<Person> selectAllPeople();
+    Optional<Person> selectPersonById(UUID id);
+    int deletePersonById(UUID id);
+    int updatePersonById(UUID id, Person person);
 }
